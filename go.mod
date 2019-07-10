a87fa69638af094613e3172e4ddd46ad1bdb5342
Les Aker <me@lesaker.org>
2019-07-10 09:02:05 +0000

    update files

--- go.mod
+++ go.mod
@@ -3,8 +3,19 @@ module github.com/akerl/feefifofum
 go 1.12
 
 require (
-	github.com/akerl/go-lambda v0.1.0
-	github.com/aws/aws-sdk-go-v2 v0.7.0
+	github.com/akerl/go-lambda v0.2.0
+	github.com/aws/aws-sdk-go v1.20.17
+	github.com/aws/aws-sdk-go-v2 v0.9.0
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/google/uuid v1.1.1
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
+	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
+	golang.org/x/tools v0.0.0-20190709211700-7b25e351ac0e // indirect
+	google.golang.org/appengine v1.6.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- main.go
+++ main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"crypto/subtle"
+	"encoding/json"
+	"fmt"
+
 	"github.com/akerl/go-lambda/apigw/events"
 	"github.com/akerl/go-lambda/mux"
 	"github.com/akerl/go-lambda/s3"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
+	s3api "github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/google/go-github/github"
+	"github.com/google/uuid"
+)
+
+const (
+	signatureHeaderName = "X-Hub-Signature"
+	tokenHeaderName     = "Authorization"
 )
 
 type config struct {
-	Queue         string
+	QueueURL      string
 	Bucket        string
 	WebhookSecret string
+	UserToken     string
 }
 
 var c config
 
 func checkSignature(req events.Request) (events.Response, error) {
-	signatureHeader := req.Headers["X-Hub-Signature"]
+	signatureHeader := req.Headers[signatureHeaderName]
 	err := github.ValidateSignature(signatureHeader, req.Body, c.WebhookSecret)
 	return events.Response{}, err
+}
+
+func checkToken(req events.Request) (events.Response, error) {
+	tokenHeader := req.Headers[tokenHeaderName]
+	expectedToken = "Bearer " + c.UserToken
+	res := subtle.ConstantTimeCompare(tokenHeader, expectedToken)
+	if res != 1 {
+		return events.Response{}, nil
+	}
+	return events.Response{}, fmt.Errorf("token incorrect")
 }
 
 func methodCheck(method string) func(events.Request) bool {
@@ -39,9 +61,104 @@ func getSqsClient() (*sqs.SQS, error) {
 }
 
 func enqueueHandler(req events.Request) (events.Response, error) {
+	obj := events.Response{
+		Headers:    req.Headers,
+		Body:       req.Body,
+		StatusCode: 200,
+	}
+
+	encodedObj, err := json.Marshal(obj)
+	if err != nil {
+		return events.Response{}, err
+	}
+
+	s3Client, err := s3.Client()
+	if err != nil {
+		return events.Response{}, err
+	}
+
+	objName, err := uuid.NewRandom()
+	if err != nil {
+		return events.Response{}, err
+	}
+
+	s3RequestInput := &s3api.PutObjectInput{
+		Bucket: aws.String(c.Bucket),
+		Key:    aws.String(objName),
+		Body:   encodedObject,
+	}
+	s3Request, err := s3Client.PutObject(s3RequestInput)
+	if err != nil {
+		return events.Response{}, err
+	}
+
+	sqsClient, err := getSqsClient()
+	if err != nil {
+		return events.Response{}, err
+	}
+
+	sqsRequestInput := &sqs.SendMessageInput{
+		QueueUrl:    aws.String(c.QueueURL),
+		MessageBody: aws.String(objName),
+	}
+	sqsRequest, err := sqsClient.SendMessage(sqsRequestInput)
+	if err != nil {
+		return events.Response{}, err
+	}
+
+	return events.Succeed(objName)
 }
 
 func dequeueHandler(req events.Request) (events.Response, error) {
+	sqsClient, err := getSqsClient()
+	if err != nil {
+		return events.Response{}, err
+	}
+
+	sqsRequestInput := &sqs.ReceiveMessageInpput{
+		QueueUrl:            c.QueueURL,
+		MaxNumberOfMessages: aws.Int64(1),
+		VisibilityTimeout:   aws.Int64(10),
+		WaitTimeSeconds:     aws.Int64(0),
+	}
+	sqsRequest, err := sqsClient.ReceiveMessage(sqsRequestInput)
+	if err != nil {
+		return events.Response{}, err
+	}
+
+	if len(result.Messages) == 0 {
+		return events.Response{StatusCode: 204}, nil
+	}
+
+	s3Client, err := s3.Client()
+	if err != nil {
+		return events.Response{}, err
+	}
+
+	s3RequestInput := &s3api.GetObjectInput{
+		Bucket: aws.String(c.Bucket),
+		Key:    aws.String(sqsRequest.MessageBody),
+	}
+	s3Request, err := s3Client.GetObject(s3RequestInput)
+	if err != nil {
+		return events.Response{}, err
+	}
+
+	obj := events.Response{}
+	err := json.Unmarshal(s3Request.Body, obj)
+	if err != nil {
+		return events.Response{}, err
+	}
+
+	_, err := svc.DeleteMessage(&sqs.DeleteMessageInput{
+		QueueUrl:      c.QueueURL,
+		ReceiptHandle: result.Messages[0].ReceiptHandle,
+	})
+	if err != nil {
+		return events.Response{}, err
+	}
+
+	return obj, nil
 }
 
 func loadConfig() {
